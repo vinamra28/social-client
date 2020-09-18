c375a0794c0abf1e54687861ae57b7aea0fe3020
vinamra28 <vinjain@redhat.com>
2020-09-18 16:05:03 +0530

    check

--- src/components/profiles/ProfileItem.js
+++ src/components/profiles/ProfileItem.js
@@ -16,9 +16,9 @@ const ProfileItem = ({
       <img src={avatar} alt='' className='round-img' />
       <div>
         <h2>{name}</h2>
-        <p>
+        {/* <p>
           {status} {company && <span> at {company}</span>}
-        </p>
+        </p> */}
         <p className='my-1'>{location && <span>{location}</span>}</p>
         <Link to={`/profile/${_id}`} className='btn btn-primary'>
           View Profile
